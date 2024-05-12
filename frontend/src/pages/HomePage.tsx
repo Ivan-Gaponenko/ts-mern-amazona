a568e447f1743c863e2f8104c168c65ac4462efc
Ivan Gaponenko <gimkiev@gmail.com>
2024-05-12 23:52:44 +0200

    Video-10-Create-Rating-ProductItem-Component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import { Col, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import { Product } from "../types/Product";
 import { useEffect, useReducer } from "react";
 import axios from "axios";
@@ -7,6 +6,7 @@ import { getError } from "../utils";
 import { ApiError } from "../types/ApiError";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import ProductItem from "../components/ProductItem";
 
 type State = {
   products: Product[];
@@ -44,7 +44,7 @@ const reducer = (state: State, action: Action) => {
 export default function HomePage() {
   const [{ loading, error, products }, dispatch] = useReducer<
   React.Reducer<State, Action>
->(reducer, initialState)
+  >(reducer, initialState)
 
 useEffect(() => {
   const fetchData = async () => {
@@ -67,15 +67,7 @@ useEffect(() => {
     <Row>
       {products.map((product) => (
         <Col key={product.slug} sm={6} md={4} lg={3}>
-          <Link to={"/product/" + product.slug}>
-            <img
-              src={product.image}
-              alt={product.name}
-              className="product-image"
-            />
-            <h2>{product.name}</h2>
-            <p>${product.price}</p>
-          </Link>
+          <ProductItem product={product} />
         </Col>
       ))}
     </Row>
