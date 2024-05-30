6e80fd0b7bb1773c30133cee4b3e286c3caf7ec2
Ivan Gaponenko <gimkiev@gmail.com>
2024-05-30 11:47:18 +0200

    Video-28-Pay-Order-By-PayPal

--- backend/src/routers/orderRouter.ts
+++ backend/src/routers/orderRouter.ts
@@ -45,3 +45,27 @@ orderRouter.post(
     }
   })
 )
+
+orderRouter.put(
+  '/:id/pay',
+  isAuth,
+  expressAsyncHandler(async (req: Request, res: Response) => {
+    const order = await OrderModel.findById(req.params.id).populate('user')
+
+    if (order) {
+      order.isPaid = true
+      order.paidAt = new Date(Date.now())
+      order.paymentResult = {
+        paymentId: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      }
+      const updatedOrder = await order.save()
+
+      res.send({ order: updatedOrder, message: 'Order Paid Successfully' })
+    } else {
+      res.status(404).send({ message: 'Order Not Found' })
+    }
+  })
+)
